7c0c1ded69e63a0c9dc5dd40f7a48e496c7c88e7
Leon <doncorleone142857@gmail.com>
2023-10-26 02:03:20 +0700

    export default

--- app/home/UploadForm.tsx
+++ app/home/UploadForm.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 
-export function UploadForm() {
+export default function UploadForm() {
   const [file, setFile] = useState<File>()
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
