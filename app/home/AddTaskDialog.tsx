7286d6f42b0c52e8e52124e27befb018c617c9b5
Leon <doncorleone142857@gmail.com>
2023-12-02 16:00:26 +0700

    refactoring and connecting to endpoint

--- app/home/AddTaskDialog.tsx
+++ app/home/AddTaskDialog.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import prisma from "@/lib/prisma";
 
 interface StylableProps{
@@ -17,52 +17,90 @@ const CloseIcon = () => {
     </svg>
   )
 }
+interface Task {
+  title: string;
+  description: string;
+  date: string;
+  time: string;
+  complete: Boolean;
+}
 
-const TaskDialog:React.FC<TaskDialogProps> = ({className, onClose}) => {
-  // async function getAllTasks(){
-  //   return await prisma.task.create({
-  //     data:{
-  //       title: taskName,
-  //       description: description,
-  //       date: date,
-  //       time: time,
+// FIXME: prisma.task.create produces an error 
+async function createTask(task : Task) {
+  console.log('Sending task to server:', task);
+  const response = await fetch('/api/addTask', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(task),
+  });
+  console.log('Received response from server:', response);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  console.log(response.json())
+  return response.json();
+}
 
-  //     },
-  //   })
-  // }
-  const [taskName, setTaskName] = useState<string>('');
+
+// FIXME: CODE COMPLEXITY IS 16
+const TaskForm:React.FC = () => {
+  const [title, setTaskName] = useState<string>('');
   const [description, setDescription] = useState<string>('');
   const [date, setDate] = useState<string>('');
   const [time, setTime] = useState<string>('');
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const task = {taskName, description, date, time}
-    console.log(task);
+    const task = {title, description, date, time, complete: false}
+  
+    try{
+      await createTask(task);
+    } catch (error){
+      console.error(error)
+    }
   }
+
+  return(
+    <form onSubmit={handleSubmit} className='flex flex-col p-4'>
+      <input type='text' onChange={(e) => setTaskName(e.target.value)}
+        required 
+        placeholder='Task name'
+        className='border border-gray-400 p-2 mb-4 rounded-lg w-full' ></input>
+      <textarea onChange={(e) => setDescription(e.target.value)} 
+        placeholder='Task description' 
+        value={description} 
+        className='border border-gray-400 p-2 mb-4 rounded-lg w-full' />
+      <div className='flex justify-between mb-4'>
+        <input type='date' onChange={(e) => setDate(e.target.value)} className="cursor-pointer"/>
+        <input type='time' onChange={(e) => setTime(e.target.value)} className="cursor-pointer"/>
+      </div>
+      <input type="submit" value="Add Task"
+        className='bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer'/>
+    </form>
+  )
+}
+
+
+// Create a new component for the dialog header
+const DialogHeader: React.FC<{ onClose: Function }> = ({ onClose }) => (
+  <div className='flex justify-between items-center px-4 py-4 border-b'>
+    <h2 className='text-2xl font-medium'>Add a new task</h2>
+    <button onClick={() => onClose()}>
+      <CloseIcon></CloseIcon>
+    </button>
+  </div>
+)
+
+const TaskDialog:React.FC<TaskDialogProps> = ({className, onClose}) => {
   return(
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
-        <div className='absolute w-full h-full bg-gray-900 opacity-50' onClick={onClose}></div>
-        <div className='absolute z-100 bg-white rounded-lg w-1/3 px-2 py-2'>
-          <div className='flex justify-between items-center px-4 py-4 border-b'>
-            <h2 className='text-2xl font-medium'>Add a new task</h2>
-            <button onClick={() => onClose()}>
-              <CloseIcon></CloseIcon>
-            </button>
-          </div>
-          <form onSubmit={handleSubmit} className='flex flex-col p-4'>
-            <input type='text' onChange={(e) => setTaskName(e.target.value)} required placeholder='Task name'
-             className='border border-gray-400 p-2 mb-4 rounded-lg w-full' />
-            <textarea onChange={(e) => setDescription(e.target.value)} placeholder='Task description' value={description} className='border border-gray-400 p-2 mb-4 rounded-lg w-full' />
-            <div className='flex justify-between mb-4'>
-              <input type='date' onChange={(e) => setDate(e.target.value)} className="cursor-pointer"/>
-              <input type='time' onChange={(e) => setTime(e.target.value)} className="cursor-pointer"/>
-            </div>
-            <input type="submit" value="Add Task"
-             className='bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer'/>
-          </form>
-        </div>
+      <div className='absolute w-full h-full bg-gray-900 opacity-50'
+       onClick={onClose}/>
+      <div className='absolute z-100 bg-white rounded-lg w-1/3 px-2 py-2'>
+        <DialogHeader onClose={onClose}/>
+        <TaskForm></TaskForm>
       </div>
+    </div>
   )
 }
 
