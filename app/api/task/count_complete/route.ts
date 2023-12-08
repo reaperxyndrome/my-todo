3e0c8308a5d5f5fd103d764cabb4a3f49e9091ab
Leon <doncorleone142857@gmail.com>
2023-12-08 23:15:59 +0700

    new route for count complete task

---
+++ app/api/task/count_complete/route.ts
@@ -0,0 +1,18 @@
+
+import prisma from '@/lib/prisma'
+import {NextResponse } from 'next/server'
+export async function GET() {
+    try {
+      const count_complete = await prisma.task.count({
+        where: {
+          complete: true,
+        },
+      });
+      const res = NextResponse.json(count_complete)
+      console.log("Task completed: ", res) 
+      return res;
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ error: 'An error occurred while fetching the tasks' });
+    }
+  }
