c8c7ffbd76c6399bcd17c615a37b95df7263d55f
Leon <doncorleone142857@gmail.com>
2023-12-14 02:10:16 +0700

    refactor the HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,106 +1,117 @@
 "use client"
 import AddTaskDialog from './AddTaskDialog';
-import {Dispatch, SetStateAction, createContext, useCallback, useEffect, useState } from 'react';
+import {useCallback, useEffect, useState } from 'react';
 import Tasks from "./Tasks"
-import { TaskProps } from './Task';
+import { RefreshTasksContext } from './context';
 interface StylableProps{
   className?: string;
 }
 
-// export const RefreshTasksContext = createContext(() => {});
-import { RefreshAddTasksContext } from './context';
+const getCompletedTaskCount = async ({setPoints}: {setPoints: React.Dispatch<React.SetStateAction<number>>}) => {
+  try {
+    const res = await fetch('/api/task/count_complete', {
+      method: 'GET',
+    })
+    // handle the error
+    if (!res.ok) throw new Error("Failed to get count of completed tasks")
+    const completedCount = await res.json()
+    // console.log(completedCount)
+    setPoints(completedCount)
+    return completedCount
+  } catch (e: any) {
+    // Handle errors here
+    console.error(e)
+    return 0; // return a default value in case of an error
+  }
+}
 
-// TODO: add a context to store refresh tasks when a new task is added
-export default function HomePage() {
-  const [refreshKey, setRefreshKey] = useState(0);
-  const [showAddTask, setShowModal] = useState(false);
+interface calculateProgressLevelProps{
+  points: number,
+  setProgress: React.Dispatch<React.SetStateAction<number>>,
+  setLevel: React.Dispatch<React.SetStateAction<number>>
+}
+
+const calculateProgressLevel = ({points, setProgress, setLevel}: calculateProgressLevelProps) => {
+  let level = 1;
+  let tempPoints = points
+  // console.log(tempPoints)
+  let requiredPoints = level
+  // console.log(requiredPoints)
+  // console.log("level: ", level)
+  // console.log("temppoints: ", tempPoints)
+  // console.log("requiredPoints: ", requiredPoints)
+  while (tempPoints >= requiredPoints) {
+    tempPoints -= requiredPoints;
+    // console.log(tempPoints)
+    level++;
+    requiredPoints = level
+    // console.log("level: ", level)
+    // console.log("temppoints: ", tempPoints)
+    // console.log("requiredPoints: ", requiredPoints)
+  }
+  const requiredPointsThisLevel = requiredPoints
+  const pointsGainedThisLevel = tempPoints
+  const progressThisLevel = Number((pointsGainedThisLevel / requiredPointsThisLevel * 100).toFixed(2))
+
+
+  // console.log("points gained in this level:", pointsGainedThisLevel)
+  // console.log("remaining points needed to level up:", remainingPointsThisLevel)
+  // console.log("required points needed to pass this level: ", requiredPointsThisLevel)
+  // console.log(progressThisLevel)
+
+  setProgress(progressThisLevel)
+  setLevel(level)
+}
+
+const Level = () => {
   const [points, setPoints] = useState(0);
   const [level, setLevel] = useState(1);
   const [progress, setProgress] = useState(0)
 
-  const handleAddTaskClick = () => {
-    setShowModal(true);
-  };
-
-  const handleModalClose = () => {
-    setShowModal(false);
-  };
-
-  const refreshTasks = useCallback(() => {
-    setRefreshKey(prevKey => prevKey + 1);
-  }, []);
-
-  const AddIcon:React.FC<StylableProps> = () => {
-    return(
-      <svg className="w-[30px] h-[30px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M9 1v16M1 9h16"/>
-      </svg>
-    )
-  }  
-  // const completedTaskCount
-  const getCompletedTaskCount = async () => {
-    try {
-      const res = await fetch('/api/task/count_complete', {
-        method: 'GET',
-      })
-      // handle the error
-      if (!res.ok) throw new Error("Failed to get count of completed tasks")
-      const completedCount = await res.json()
-      // console.log(completedCount)
-      setPoints(completedCount)
-      return completedCount
-    } catch (e: any) {
-      // Handle errors here
-      console.error(e)
-      return 0; // return a default value in case of an error
-    }
-  }
-  // const taskCount = getCompletedTaskCount();
-  
-  
   useEffect(() => {
     // console.log("Points changed")
-    // TODO: fix leveling system
-    const calculateProgressLevel = () => {
-      let level = 1;
-      let tempPoints = points
-      // console.log(tempPoints)
-      let requiredPoints = level
-      // console.log(requiredPoints)
-      console.log("level: ", level)
-      console.log("temppoints: ", tempPoints)
-      console.log("requiredPoints: ", requiredPoints)
-      while (tempPoints >= requiredPoints) {
-        tempPoints -= requiredPoints;
-        // console.log(tempPoints)
-        level++;
-        requiredPoints = level
-        console.log("level: ", level)
-        console.log("temppoints: ", tempPoints)
-        console.log("requiredPoints: ", requiredPoints)
-      }
-      const requiredPointsThisLevel = requiredPoints
-      const pointsGainedThisLevel = tempPoints
-      const progressThisLevel = Number((pointsGainedThisLevel / requiredPointsThisLevel * 100).toFixed(2))
-
- 
-      // console.log("points gained in this level:", pointsGainedThisLevel)
-      // console.log("remaining points needed to level up:", remainingPointsThisLevel)
-      // console.log("required points needed to pass this level: ", requiredPointsThisLevel)
-      // console.log(progressThisLevel)
-
-      setProgress(progressThisLevel)
-      setLevel(level)
-    }
-    calculateProgressLevel()
+    getCompletedTaskCount({setPoints})
+    calculateProgressLevel({points, setProgress, setLevel})
   }, [points])
 
-  useEffect(() => {
-    getCompletedTaskCount()
-      .then((count) => setPoints(count))
-  }, [])
+  return(
+    <div className='flex flex-col items-center'>
+      <h2 className='text-3xl mb-2'>Level {level}</h2>
+      <div className='flex items-center mb-2 w-[300px] h-9 bg-black p-1 rounded-lg'>
+        <div className='flex items-center h-6 bg-green-400 rounded-md pl-2 py-[2px] transition-[width]' style={{width: `${progress}%`}}>
+          <p className='text-white'>{progress}%</p>
+        </div>
+      </div>
+      <h2 className='text-2xl mb-5'>Total points: {points}</h2>  
+    </div>
+  )
+}
 
+const AddIcon:React.FC<StylableProps> = () => {
+  return(
+    <svg className="w-[30px] h-[30px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M9 1v16M1 9h16"/>
+    </svg>
+  )
+}  
+
+const AddButton = ({setShowModal}: { setShowModal: (show: boolean) => void }) => {
+  return(
+    <div className='flex gap-x-9 items-center justify-center mb-[2rem] bg-[grey] hover:bg-[white] text-[white] hover:text-black hover:border-[5px] py-[1rem] px-[1rem] rounded-lg cursor-pointer' onClick={() => setShowModal(true)}>        
+      <AddIcon />
+      <h2 className='text-2xl font-medium'>Add a new task</h2>
+    </div>
+  )
+}
+
+export default function HomePage() {
+  const [refreshTaskKey, setRefreshTaskKey] = useState(0);
+  const [showAddTask, setShowModal] = useState(false);
   
+  const refreshTasks = useCallback(() => {
+    setRefreshTaskKey(prevKey => prevKey + 1);
+  }, []);
+
   return (
     <main className='flex flex-col justify-center items-center py-[4rem] '>
       <div className='mb-[4rem]'>
@@ -113,26 +124,17 @@ export default function HomePage() {
       </div>
       
       {/* TODO: Make the progress bar animated */}
-      <h2 className='text-3xl mb-2'>Level {level}</h2>
-      <div className='flex items-center mb-2 w-[300px] h-9 bg-black p-1 rounded-lg'>
-        <div className='flex items-center h-6 bg-green-400 rounded-md pl-2 py-[2px] transition-[width]' style={{width: `${progress}%`}}>
-          <p className='text-white'>{progress}%</p>
-        </div>
-      </div>
-      <h2 className='text-2xl mb-5'>Total points: {points}</h2>
-      
-      <div className='flex gap-x-9 items-center justify-center mb-[2rem] bg-[grey] hover:bg-[white] text-[white] hover:text-black hover:border-[5px] py-[1rem] px-[1rem] rounded-lg cursor-pointer' onClick={handleAddTaskClick}>        
-        <AddIcon />
-        <h2 className='text-2xl font-medium'>Add a new task</h2>
-      </div>
-      <RefreshAddTasksContext.Provider value={refreshTasks}>
-        <Tasks key={refreshKey}/>
+      {/* TODO: Add key to level component (make context later) */}
+      <Level/>
+      <AddButton setShowModal={setShowModal}></AddButton>
+      <RefreshTasksContext.Provider value={refreshTasks}>
+        <Tasks key={refreshTaskKey}/>
         
         {showAddTask && (
-          <AddTaskDialog onClose={handleModalClose}></AddTaskDialog>
+          <AddTaskDialog onClose={() => setShowModal(false)}></AddTaskDialog>
         )}
         
-      </RefreshAddTasksContext.Provider>
+      </RefreshTasksContext.Provider>
       
     </main>
   )
