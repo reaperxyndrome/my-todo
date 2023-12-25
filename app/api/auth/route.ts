8d4f75cd00c5a0f138e684eca0aba1dd8a253bf0
Leon <doncorleone142857@gmail.com>
2023-12-26 04:33:27 +0700

    fix for deployment issues

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { authOptions } from "./[...nextauth]/route";
+// import { authOptions } from "./[...nextauth]/route";
+import { authOptions } from "@/app/auth";
 
 export async function GET(req: Request) {
     const session = await getServerSession(authOptions);
---
+++ app/auth.ts
@@ -0,0 +1,79 @@
+import prisma from '@/lib/prisma';
+import { compare } from 'bcrypt';
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions: NextAuthOptions = {
+    session:{
+        strategy: 'jwt'
+    },
+    providers: [
+      CredentialsProvider({
+        // The name to display on the sign in form (e.g. "Sign in with...")
+        name: "Credentials",
+        // `credentials` is used to generate a form on the sign in page.
+        // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+        // e.g. domain, username, password, 2FA token, etc.
+        // You can pass any HTML attribute to the <input> tag through the object.
+        credentials: {
+          email: { label: "Email", type: "email", placeholder: "jsmith@example.com" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials, req) {
+          // Add logic here to look up the user from the credentials supplied
+          if(!credentials?.password || !credentials?.email){
+            return null
+          }
+  
+          const user = await prisma.user.findUnique({
+            where:{
+              email: credentials.email
+            }
+          })
+  
+          if (!user){
+            return null
+          }
+          
+          const isPasswordValid = await compare(credentials.password, user.password)
+  
+          if(!isPasswordValid){
+            return null
+          }
+  
+          return {
+            id: user.id + "",
+            email: user.email,
+            name: user.name,
+            randomKey: "hello there"
+          }
+        }
+      })
+    ],
+    callbacks: {
+      session: async ({session, token}) => {
+        console.log("Session Callback", {session, token})
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            id: token.id,
+            randomKey: token.randomKey
+          }
+        }
+      },
+      jwt: async ({token, user}) => {
+        console.log("JWT Callback", {token, user})
+        if (user) {
+          const u = user as unknown as any
+          return {
+            ...token,
+            id: u.id,
+            randomKey: u.randomKey
+          }
+        }
+        return token
+      }
+    },
+  }
+  
