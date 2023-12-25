de04998a15455231c249eba0c9c8f29922576919
Leon <doncorleone142857@gmail.com>
2023-12-25 21:22:30 +0700

    added validation logic for credentials auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import prisma from "@/lib/prisma";
+import { compare } from "bcrypt";
 import NextAuth, {type NextAuthOptions} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -14,21 +16,35 @@ export const authOptions: NextAuthOptions = {
           // e.g. domain, username, password, 2FA token, etc.
           // You can pass any HTML attribute to the <input> tag through the object.
           credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
+            email: { label: "Email", type: "email", placeholder: "jsmith@example.com" },
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
             // Add logic here to look up the user from the credentials supplied
-            const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-      
-            if (user) {
-              // Any object returned will be saved in `user` property of the JWT
-              return user
-            } else {
-              // If you return null then an error will be displayed advising the user to check their details.
+            if(!credentials?.password || !credentials?.email){
               return null
-      
-              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+            }
+
+            const user = await prisma.user.findUnique({
+              where:{
+                email: credentials.email
+              }
+            })
+
+            if (!user){
+              return null
+            }
+            
+            const isPasswordValid = await compare(credentials.password, user.password)
+
+            if(!isPasswordValid){
+              return null
+            }
+
+            return {
+              id: user.id + "",
+              email: user.email,
+              name: user.name
             }
           }
         })
