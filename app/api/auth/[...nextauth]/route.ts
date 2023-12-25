c15e410c2c0265a15a5bafcb2df4168f17216631
Leon <doncorleone142857@gmail.com>
2023-12-26 03:28:12 +0700

    adding more info to JWT token, passing token information to session

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import { User } from "@prisma/client";
 import { compare } from "bcrypt";
 import NextAuth, {type NextAuthOptions} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -44,11 +45,37 @@ export const authOptions: NextAuthOptions = {
         return {
           id: user.id + "",
           email: user.email,
-          name: user.name
+          name: user.name,
+          randomKey: "hello there"
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    session: async ({session, token}) => {
+      console.log("Session Callback", {session, token})
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey
+        }
+      }
+    },
+    jwt: async ({token, user}) => {
+      console.log("JWT Callback", {token, user})
+      if (user) {
+        const u = user as unknown as any
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey
+        }
+      }
+      return token
+    }
+  },
 }
 
 const handler = NextAuth(authOptions)
