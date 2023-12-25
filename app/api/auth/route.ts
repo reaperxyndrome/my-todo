6262ed2ff400a4b743fec334ba44756508defc4b
Leon <doncorleone142857@gmail.com>
2023-12-26 04:13:42 +0700

    dummy session api route

---
+++ app/api/auth/route.ts
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "./[...nextauth]/route";
+
+export async function GET(req: Request) {
+    const session = await getServerSession(authOptions);
+
+    // if (!session) {
+    //     return new NextResponse(JSON.stringify({error: "unauthorized"}), {
+    //         status: 401
+    //     });
+    // }
+    console.log("GET SESSION API", session)
+    return NextResponse.json({authenticated: !!session})
+}
