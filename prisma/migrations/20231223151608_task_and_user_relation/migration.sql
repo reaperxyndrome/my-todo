18fe40951e2787a2f08a6ad1d16e872afe471857
Leon <doncorleone142857@gmail.com>
2023-12-25 17:55:45 +0700

    modified prisma schema for credentials authentication

---
+++ prisma/migrations/20231223151608_task_and_user_relation/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "userId" UUID;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
