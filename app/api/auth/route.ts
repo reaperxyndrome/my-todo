a54e079cb83fae6d59be695ccf0df29d67e86fb1
Leon <doncorleone142857@gmail.com>
2023-12-26 18:33:38 +0700

    dummy get server session api added error code

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -6,11 +6,11 @@ import { authOptions } from "@/app/auth";
 export async function GET(req: Request) {
     const session = await getServerSession(authOptions);
 
-    // if (!session) {
-    //     return new NextResponse(JSON.stringify({error: "unauthorized"}), {
-    //         status: 401
-    //     });
-    // }
+    if (!session) {
+        return new NextResponse(JSON.stringify({error: "unauthorized"}), {
+            status: 401
+        });
+    }
     console.log("GET SESSION API", session)
     return NextResponse.json({authenticated: !!session})
 }
