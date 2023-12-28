1b013047558f212dea8e7ae1d49be992de14fb65
Leon <doncorleone142857@gmail.com>
2023-12-29 03:09:02 +0700

    simple bug fix

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -7,7 +7,7 @@ export async function getUser() {
     const session = await getServerSession(authOptions)
     const user = session?.user as User
     console.log(user)
-    return { userId: user.id, username: user.name }
+    return { userId: user.id, username: user.username }
   } catch (error) {
     console.error(error)
     throw error
